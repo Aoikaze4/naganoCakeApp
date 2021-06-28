6863b5c8e4f77defe68b1803db04fdf77e4bfeb7
Aoikaze4 <kaze4border@gmail.com>
2021-06-28 15:08:40 +0000

    [fix]root

--- nagano_cake/config/routes.rb
+++ nagano_cake/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+  get 'top', to: 'homes#top'
   get 'about', to: 'homes#about'
   root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
