4b3c17aeaf2a6243c13398f06acd17b9ee14186c
Aoikaze4 <kaze4border@gmail.com>
2021-07-01 14:05:11 +0000

    [update]remake customer,admin models and add header

--- nagano_cake/app/controllers/admin/admins_controller.rb
+++ nagano_cake/app/controllers/admin/admins_controller.rb
@@ -1,2 +1,4 @@
 class Admin::AdminsController < ApplicationController
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
--- nagano_cake/app/controllers/public/customers_controller.rb
+++ nagano_cake/app/controllers/public/customers_controller.rb
@@ -1,2 +1,4 @@
 class Public::CustomersController < ApplicationController
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
