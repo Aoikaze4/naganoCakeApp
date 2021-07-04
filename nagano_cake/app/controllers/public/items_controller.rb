af4480376b2368458d984146cbf156c12df0527c
Aoikaze4 <kaze4border@gmail.com>
2021-07-04 06:06:01 +0000

    [fix]Item.count

--- nagano_cake/app/controllers/public/items_controller.rb
+++ nagano_cake/app/controllers/public/items_controller.rb
@@ -1,6 +1,7 @@
 class Public::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page]).reverse_order
+    @items_count = Item.count
     ##@acitve_items = Item.where(is_active: "True") ←アクティブのもののみ使用する場合
   end
 
