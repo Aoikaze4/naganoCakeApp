4b5721ff3134105ae001f2d2f456e09f4e7f17b2
Aoikaze4 <kaze4border@gmail.com>
2021-07-18 09:52:14 +0000

    [update]added edit action,update action,edit.html.erb to Admin/Items

--- nagano_cake/app/controllers/admin/items_controller.rb
+++ nagano_cake/app/controllers/admin/items_controller.rb
@@ -19,6 +19,21 @@ class Admin::ItemsController < ApplicationController
     @genre = Genre.find(@item.genre_id)
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    @genres_name_all = Genre.all
+    @genre = Genre.find(@item.genre_id)
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item.id), notice: '変更を保存しました。'
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :genre_id, :introduction, :price, :is_active, :image)
