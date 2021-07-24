185b00292ad217283456274295a257a81d8c2deb
Aoikaze4 <kaze4border@gmail.com>
2021-07-24 07:42:24 +0000

    [fix]deleted i18n from config/application.rb,because didnt installed required gem

--- nagano_cake/app/models/order.rb
+++ nagano_cake/app/models/order.rb
@@ -4,6 +4,8 @@ class Order < ApplicationRecord
   belongs_to :customer
 
   enum order_status: { wait: 0, paid: 1, prepaer: 2, send: 3 }
+  #0→注文待機中　1→支払い済み 2→発送準備中　3→発送済み
 
   enum payment_method: {credit_card: 0, bank: 1}
+  #0→クレジットカード 1→銀行振込
 end
