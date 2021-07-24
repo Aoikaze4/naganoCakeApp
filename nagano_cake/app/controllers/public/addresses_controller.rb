0ba9fb5bfdfc1c678d80f5c61236ce9d95eb60d1
Aoikaze4 <kaze4border@gmail.com>
2021-07-24 08:05:46 +0000

    [update]address index.html.erb, some actions to addressesController

--- nagano_cake/app/controllers/public/addresses_controller.rb
+++ nagano_cake/app/controllers/public/addresses_controller.rb
@@ -1,2 +1,15 @@
 class Public::AddressesController < ApplicationController
+  def index
+    @addresses = Address.where(customer_id: current_customer.id)
+  end
+
+  def new
+    @address = Address.new(params[:id])
+  end
+
+
+  private
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address)
+  end
 end
