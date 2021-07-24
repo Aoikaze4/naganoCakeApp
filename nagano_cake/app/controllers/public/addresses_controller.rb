ff9672ae10ed46ea82f25608d6c1701d7227e9e5
Aoikaze4 <kaze4border@gmail.com>
2021-07-24 12:48:57 +0000

    [update]addresses'controller is added create,new actions

--- nagano_cake/app/controllers/public/addresses_controller.rb
+++ nagano_cake/app/controllers/public/addresses_controller.rb
@@ -4,12 +4,19 @@ class Public::AddressesController < ApplicationController
   end
 
   def new
-    @address = Address.new(params[:id])
+    @address = Address.new
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.save
+    redirect_to addresses_path
   end
 
 
   private
   def address_params
-    params.require(:address).permit(:name, :postal_code, :address)
+    params.permit(:name, :postal_code, :address, :customer_id)
   end
+  #モデル名＝カラム名であるためrequireを抜いてみる
 end
