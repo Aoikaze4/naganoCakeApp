a45c6862624bd3629262e99c7884a7f38dc841ba
Aoikaze4 <kaze4border@gmail.com>
2021-07-24 07:35:27 +0000

    [update]added enum to order model

--- nagano_cake/app/controllers/public/orders_controller.rb
+++ nagano_cake/app/controllers/public/orders_controller.rb
@@ -1,2 +1,9 @@
 class Public::OrdersController < ApplicationController
+  def new
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:postal_code, :address, :name, :order_status, :payment_method)
+  end
 end
--- nagano_cake/app/models/order.rb
+++ nagano_cake/app/models/order.rb
@@ -2,4 +2,8 @@ class Order < ApplicationRecord
   has_many :order_items
 
   belongs_to :customer
+
+  enum order_status: { wait: 0, paid: 1, prepaer: 2, send: 3 }
+
+  enum payment_method: {credit_card: 0, bank: 1}
 end
