9933fdb2e15707e27900419bdd4fd436c7e85b67
Aoikaze4 <kaze4border@gmail.com>
2021-08-04 06:20:05 +0000

    [update]added beforeaction active_item to public/items/showcontroller

--- nagano_cake/app/controllers/public/items_controller.rb
+++ nagano_cake/app/controllers/public/items_controller.rb
@@ -1,9 +1,18 @@
 class Public::ItemsController < ApplicationController
   before_action :authenticate_customer!
+  before_action :active_item, only: [:show]
+
+  def active_item
+    @item = Item.find(params[:id])
+    unless @item.is_active == true
+      redirect_to items_path, alert: "指定の商品は現在販売停止中です。"
+    end
+  end
 
   def index
     active_items = Item.where(is_active: true)
     @items = active_items.page(params[:page]).reverse_order
+    @items_count = active_items.count
   end
 
   def show
