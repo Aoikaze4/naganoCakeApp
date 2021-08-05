3d7ca3688d5c5fa72bbf030f286fd3a4f3be1b6c
Aoikaze4 <kaze4border@gmail.com>
2021-08-05 01:20:30 +0000

    [update]admins order,order_item controller was added is added status update function

--- nagano_cake/app/controllers/admin/order_items_controller.rb
+++ nagano_cake/app/controllers/admin/order_items_controller.rb
@@ -3,7 +3,15 @@ class Admin::OrderItemsController < ApplicationController
 
   def update
     order_item = OrderItem.find(params[:id])
+    order = order_item.order
     if order_item.update(order_item_params)
+      if order_item.make_status == "製作中"
+        order.update(order_status: 2)
+      elsif order.order_items.count == order.order_items.where(make_status: "製作完了").count
+        #すべてのアイテムが製作完了になったら
+        order.update(order_status: 3)
+      end
+
       redirect_to admin_order_path(order_item.order.id), notice: "製作ステータスの変更が完了しました。"
     else
       render 'admin/orders/show'
--- nagano_cake/app/controllers/admin/orders_controller.rb
+++ nagano_cake/app/controllers/admin/orders_controller.rb
@@ -12,10 +12,17 @@ class Admin::OrdersController < ApplicationController
 
   def update
     order = Order.find(params[:id])
+    order_items = order.order_items
     if order.update(order_params)
+      if order.order_status == "支払い済み"
+        order_items.update_all(make_status: 1)
+      end
+
       redirect_to admin_order_path(params[:id]), notice: '注文ステータスの変更が完了しました。'
+
     else
       render :show
+
     end
   end
 
